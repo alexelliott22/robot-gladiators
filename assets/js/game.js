bbc45caa4d7653d74898384a6998242eb47c3cad
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-04-28 18:31:13 -0400

    3.5.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,38 @@
+
+
+var fightOrSkip = function() {
+    //ask player if they'd like to fight or skip using fightOrSkip function 
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    // Conditional Recursive Function Call
+    if (promptFight === "" || promptFight === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      return fightOrSkip();
+}
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "skip") {
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you want to skip?");
+
+        //if yes, leave fight
+        if(confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Godbye.");
+            //subtract money for skipping
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+            return true;
+        }
+    }
+    return false;
+}
+
 //fight function 
 var fight = function(enemy) {
     //repeat and execute as long as the enemy is alive
     while(playerInfo.health > 0 && enemy.health > 0) {
-           //alert players they are starting the round
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'fight' or 'skip' to choose.")
-
-        //if player chooses to skip
-        if(promptFight === "skip" || promptFight === "SKIP") {
-            window.alert(playerInfo.name + " has chosen to skip the battle.");
-            
-            //confirm player wants to skip
-            var confirmSkip = window.confirm("Are you sure you want to quit?");
-
-            //if yes (true), leave the fight 
-            if (confirmSkip) {
-                window.alert(playerInfo.name + " has decided to skip this fight. Goodbye.");
-                //subtract money from player
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money);
-                break;
-            }
-        }
+       
+        //fight or skip round  
+        fightOrSkip();
 
         //Subtract the value of `playerInfo.attack` from the value of `enemy.health` and use that result to update the value in the `enemy.health` variable
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -204,6 +216,7 @@ var enemyInfo = [{
 }
 ];
 
+
+
 // start first game when page loads
 startGame();
-
