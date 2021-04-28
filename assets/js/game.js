233e96efbf50ded7b1a2dea69858efcbed9d4c68
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-04-28 16:20:44 -0400

    3.4.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,14 +25,15 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye.");
                 //subtract money from player
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -52,7 +53,8 @@ var fight = function(enemyName) {
         }
 
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack -3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -70,6 +72,12 @@ var fight = function(enemyName) {
     }
 };
 
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min +1) + min);
+
+    return value;
+};
+
 //function to start new game
 var startGame = function() {
     //reset player stats
@@ -83,7 +91,7 @@ var startGame = function() {
         if(playerHealth > 0) {
             window.alert ("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             if  (i < enemyNames.length - 1 && playerHealth > 0) {
                 var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
@@ -172,3 +180,4 @@ var shop = function() {
 
 // start first game when page loads
 startGame();
+
